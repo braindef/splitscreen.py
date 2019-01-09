b516f44931e025209f0ee54c600927b81e3f4bf3
landev <landev@mail>
2019-01-09 06:33:32 +0100

    tempsave

--- splitscreen.py
+++ splitscreen.py
@@ -24,8 +24,10 @@ class SplitScreen:
     curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_CYAN)
     curses.init_pair(2,curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(3,curses.COLOR_WHITE, curses.COLOR_RED)
+    curses.init_pair(4,curses.COLOR_WHITE, curses.COLOR_BLUE)
     SplitScreen.highlightText = curses.color_pair( 3 )
     SplitScreen.highlightWindow = curses.color_pair( 1 )
+    SplitScreen.coloredStatusbox = curses.color_pair( 4 )
     SplitScreen.normalText = curses.A_NORMAL
     SplitScreen.refresh()
     SplitScreen.keyhandler()
@@ -36,23 +38,26 @@ class SplitScreen:
     SplitScreen.rows = int(y)
     SplitScreen.cols = int(x)
     SplitScreen.middleCol = int(SplitScreen.cols/2)
+    SplitScreen.middleRow = int(SplitScreen.rows/2)
+    SplitScreen.statusRow = int(SplitScreen.rows-6)
 
 
   def refresh():
       SplitScreen.getGeometry()
       SplitScreen.screen.clear()
       SplitScreen.screen.refresh()
-      SplitScreen.leftBox = curses.newwin( SplitScreen.max_row + 20, SplitScreen.middleCol-1, 1, 1 )
-      SplitScreen.rightBox = curses.newwin( SplitScreen.max_row + 20, SplitScreen.middleCol, 1, SplitScreen.middleCol )
-      SplitScreen.leftBox.box()
-      SplitScreen.rightBox.box()
       SplitScreen.screen.border( 0 )
       SplitScreen.screen.refresh()
+
+      SplitScreen.leftBox = curses.newwin( SplitScreen.rows-6, SplitScreen.middleCol-1, 1, 1 )
+      SplitScreen.rightBox = curses.newwin( SplitScreen.rows-6, SplitScreen.middleCol, 1, SplitScreen.middleCol )
+      SplitScreen.statusBox = curses.newwin( 4, SplitScreen.cols-2, SplitScreen.statusRow+1, 1 )
+      SplitScreen.leftBox.box()
+      SplitScreen.rightBox.box()
 
       if SplitScreen.activeBox == 0:
         SplitScreen.leftBox.bkgd(SplitScreen.highlightWindow)
         SplitScreen.rightBox.bkgd(SplitScreen.normalText)
-
       if SplitScreen.activeBox == 1:
         SplitScreen.rightBox.bkgd(SplitScreen.highlightWindow)
         SplitScreen.leftBox.bkgd(SplitScreen.normalText)
@@ -63,11 +68,13 @@ class SplitScreen:
       SplitScreen.populate(SplitScreen.rightBox, SplitScreen.rightHighlightedCursor)
       SplitScreen.rightBox.border(0)
       SplitScreen.rightBox.refresh()
+        
+      SplitScreen.statusBox.box()
+      SplitScreen.statusBox.border(0)
+      SplitScreen.statusBox.bkgd(SplitScreen.coloredStatusbox)
+      SplitScreen.statusBox.addstr(1, 2, "Press [TAB] to switch the side", SplitScreen.coloredStatusbox)
+      SplitScreen.statusBox.refresh()
       
-
-      
-
-
   def populate(box, selected):
     strings = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n" ]
     i = 0
@@ -109,6 +116,7 @@ class SplitScreen:
           pass
         if x == ord('\t'):
           SplitScreen.switchBox()
+
 
         SplitScreen.refresh()
         #SplitScreen.screen.refresh()
